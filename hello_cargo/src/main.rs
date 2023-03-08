6056ae603e58a930a8cca06fbdcb18b8f2a22c55
rafael.freitas@trf1.jus.br <rafael.freitas@trf1.jus.br>
2023-03-08 15:17:15 -0300

    Terminada sessão 1.3. Hello, Cargo!

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+    cargo new: cria novo projeto
+    cargo build: builda e gera executável dentro de target
+        -- release: builda para produção
+    cargo run: builda e roda o programa
+    cargo check: verifica o código sem gerar executável
+*/
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+D:\workspace-pess\rust-book\hello_cargo\target\debug\hello_cargo.exe: D:\workspace-pess\rust-book\hello_cargo\src\main.rs
